b6361db12696af57b943d3ebe9516c6888bdd174
ch96 <ch96@zcsmart.com>
2018-02-22 11:17:34 +0800

    first commit

---
+++ src/main/java/com/zk/demo/ZookeeperJavaApiApplication.java
@@ -0,0 +1,12 @@
+package com.zk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperJavaApiApplication.class, args);
+	}
+}
